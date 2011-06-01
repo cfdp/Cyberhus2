72a022c4b1d7bd0de0d880115fbff9e9f3bee5f8
Kreativt Team <kreativt-team@cyberhus.dk>
2011-06-01 15:09:05 +0200

    added new graphics, adjust css and js, add region to page templates

--- sites/all/modules/custom/cyberchatep/js/chat_update.js
+++ sites/all/modules/custom/cyberchatep/js/chat_update.js
@@ -6,7 +6,7 @@ $(document).ready(function (){
 	
 	 //Chat entry point panel slide down
     $("#ccep_panel").hide();
-    $("#ccep_login").toggle(
+    $("#ccep_status").toggle(
         function(){
             $("#ccep_panel").slideDown();
         },
@@ -16,17 +16,15 @@ $(document).ready(function (){
      )
 
 	//on this url the chatep-module has a menu callback that delivers the json-data
-	var chat_update_url = "cyberchatep/returntext"; 
+	var chat_update_url = "http://cyberhus.devel:8082/cyberchatep/returntext"; 
 
-	//$('#green, #yellow, #red').hide();
 	function updateChat(){
 		//alert('hey');
-
 		var chatUpdated = function (data){
 			//update the chat
-			$('#ccep_login').html(data.login_message);
+			$('#ccep_status').html(data.login_message);
 			$('#ccep_desc').html(data.welcome_message);
-			chat_update_url = "cyberchatep/returntext/"+data.status;
+			chat_update_url = data.path+"/cyberchatep/returntext/"+data.status;
 
 		}
 		 //make AJAX call
@@ -44,44 +42,3 @@ $(document).ready(function (){
 	setInterval(updateChat, 4000);
 	
 });
-
-	/* if (window.XMLHttpRequest){
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		var xmlhttp;
-		xmlhttp = new XMLHttpRequest();
-	}else{
-		// code for IE6, IE5
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange = function(){
-		if(xmlhttp.readyState < 4){
-			//document.getElementById('feedback'+id).innerHTML = "<img src=\"images/ajax_loader.gif\" alt=\"loader\" width=\"16\" height=\"16\" />";
-		}
-		if (xmlhttp.readyState==4 && xmlhttp.status==200){
-			var response = xmlhttp.responseText;
-			var chat = jQuery.parseJSON(response);
-			var state = chat.state;
-			var tab_link = chat.link;
-			
-
-			
-			if(state == 'red'){
-				$('#green, #yellow, #red').hide();
-				$('#red').show();
-				$('.link').attr('href',tab_link);
-			}
-			if(state == 'green'){
-				$('#green, #yellow, #red').hide();
-				$('#green').show();
-			}
-			if(state == 'yellow'){
-				$('#green, #yellow, #red').hide();
-				$('#yellow').show();
-				$('.link').attr('href',tab_link);
-			}
-		}
-	}
-	
-	xmlhttp.open("GET", "http://chat.cybhus.dk/chat_status.php", true);
-	xmlhttp.send();
-	*/
